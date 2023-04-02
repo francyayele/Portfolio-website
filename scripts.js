db0c333f2aa7625059d2bf1606a6042ff6918ecd
Fransi <74135451+francyayele@users.noreply.github.com>
2023-04-02 20:42:44 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,28 @@
 
-    var imageSlider = document.querySelector(".image-slider"),
-        sliderWrapper = document.querySelector(".slider-wrapper"),
-        prevBtn = document.querySelector(".prev"),
-        nextBtn = document.querySelector(".next"),
-        currentSlide = 0;
+    /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
-    nextBtn.addEventListener("click", function() {
-        currentSlide++;
-        if (currentSlide > 5) {
-            currentSlide = 0;
-        }
-        sliderWrapper.style.transform = "translateX(-" + currentSlide*100 + "%)";
-    });
+/*=============== SWIPER POPULAR ===============*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-    prevBtn.addEventListener("click", function() {
-        currentSlide--;
-        if (currentSlide < 0) {
-            currentSlide = 5;
-        }
-        sliderWrapper.style.transform = "translateX(-" + currentSlide*100 + "%)";
-    });
-
-    const header = document.querySelector("header");
+const header = document.querySelector("header");
 
     window.addEventListener("scroll", function() {
         header.classList.toggle("sticky", window.scrollY > 0);
@@ -40,13 +41,121 @@
         navlist.classList.remove('active');
     };
 
-    const sr = ScrollReveal({
-        distance: '25px',
-        duration: 2500,
-        reset: true
-    })
-    sr.reveal('.home-img', { delay: 190, origin: 'top' })
-    sr.reveal('.home-text', { delay: 250, origin: 'bottom' })
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
-    sr.reveal('.about-img,.services,.portfolio,.contact', { delay: 200, origin: 'bottom' })
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true
+})
+
+sr.reveal(`.home, skill-container, .testimonial , .footer-basic `)
+sr.reveal(`.home-text, .about-text`, { delay: 500 })
+sr.reveal(`.home__search`, { delay: 600 })
+sr.reveal(`.home__value`, { delay: 700 })
+sr.reveal(`.contact, .projects, .skills, .testimonials`, { delay: 800, origin: 'bottom' })
+sr.reveal(`.logo`, { interval: 100 })
+sr.reveal(`.about-img`, { origin: 'left' })
+sr.reveal(`.home-img`, { origin: 'right' })
+
+const options = {
+  startAngle: -1.55,
+  size: 150,
+  value: 0.85,
+  fill: { gradient: ["#7861f4"] },
+};
+
+$(".circle .bar")
+  .circleProgress(options)
+  .on("circle-animation-progress", function (event, progress, stepValue) {
+    $(this)
+      .parent()
+      .find("span")
+      .text(String(stepValue.toFixed(2).substr(2)) + "%");
+  });
+$(".a .bar").circleProgress({
+  value: 0.9,
+});
+$(".b .bar").circleProgress({
+  value: 0.75,
+});
+
+$(".c .bar").circleProgress({
+  value: 0.8,
+});
+    
+$(".d .bar").circleProgress({
+  value: 0.8,
+});
+$(".e .bar").circleProgress({
+  value: 0.85,
+});
+$(".f .bar").circleProgress({
+  value: 0.8,
+});    
+    
+$('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 2500,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+  
+
+});
+    
